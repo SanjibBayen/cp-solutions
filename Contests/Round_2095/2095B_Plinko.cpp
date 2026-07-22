ba8eda7177ef4a70431088e7cb42a38dd1841118
Sanjib Bayen <112533992+SanjibBayen@users.noreply.github.com>
2026-07-23 03:54:46 +0530

    Update: [CF] 2095B: Plinko

--- Contests/Round_2095/2095B_Plinko.cpp
+++ Contests/Round_2095/2095B_Plinko.cpp
@@ -1,4 +1,5 @@
 /*
+ * -----------------------------------------------------------------------------
  * Platform:    Codeforces
  * Problem:     2095B - Plinko
  * Contest ID:  2095
@@ -6,11 +7,11 @@
  * Language:    C++23 (GCC 14-64, msys2)
  * Runtime:     31 ms
  * Memory:      100 KB
- * Difficulty:  None
+ * Tags:        *special, games, interactive
  * Solved:      2026-07-20 04:35:23
  * URL:         https://codeforces.com/problemset/problem/2095/B
+ * -----------------------------------------------------------------------------
  */
-
 #include <iostream>
 #include <string>
 
