f1b2312212f779038444d33373ee0f4c0d5a0365
Sanjib Bayen <112533992+SanjibBayen@users.noreply.github.com>
2026-07-23 00:19:13 +0530

    [CF] 546A: Soldier and Bananas

---
+++ Practice/0800-1200/546A_Soldier_and_Bananas.cpp
@@ -0,0 +1,41 @@
+/*
+ * Platform:    Codeforces
+ * Problem:     546A - Soldier and Bananas
+ * Contest ID:  546
+ * Verdict:     OK
+ * Language:    C++23 (GCC 14-64, msys2)
+ * Runtime:     31 ms
+ * Memory:      100 KB
+ * Difficulty:  800
+ * Solved:      2026-07-20 20:28:24
+ * URL:         https://codeforces.com/problemset/problem/546/A
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int k, n, w;
+    cin>>k>>n>>w;
+    int x, cost =0;
+    for(int i = 1; i<= w; i++)
+    {
+        cost += i * k;
+    }
+    x = cost - n;
+    if(cost > n)
+        cout << x << endl;
+    else
+        cout << 0 << endl;
+    
+}
+
+
+
+
+int main() {
+
+	    solve();
+	return 0;
+
+}
