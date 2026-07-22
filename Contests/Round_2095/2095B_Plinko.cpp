f815c38ad1cc8a976f1f584ccc087b9516103adf
Sanjib Bayen <112533992+SanjibBayen@users.noreply.github.com>
2026-07-23 00:19:10 +0530

    [CF] 2095B: Plinko

---
+++ Contests/Round_2095/2095B_Plinko.cpp
@@ -0,0 +1,30 @@
+/*
+ * Platform:    Codeforces
+ * Problem:     2095B - Plinko
+ * Contest ID:  2095
+ * Verdict:     OK
+ * Language:    C++23 (GCC 14-64, msys2)
+ * Runtime:     31 ms
+ * Memory:      100 KB
+ * Difficulty:  None
+ * Solved:      2026-07-20 04:35:23
+ * URL:         https://codeforces.com/problemset/problem/2095/B
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s;
+    int x;
+    if (cin >> s >> x) { 
+        cout << 143 << "\n";
+    }
+    
+    return 0;
+}
