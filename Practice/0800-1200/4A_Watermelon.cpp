627c20acff269c185ca8cf9b26a6fc051087a1fa
Sanjib Bayen <112533992+SanjibBayen@users.noreply.github.com>
2026-07-23 00:19:21 +0530

    [CF] 4A: Watermelon

---
+++ Practice/0800-1200/4A_Watermelon.cpp
@@ -0,0 +1,29 @@
+/*
+ * Platform:    Codeforces
+ * Problem:     4A - Watermelon
+ * Contest ID:  4
+ * Verdict:     OK
+ * Language:    C++23 (GCC 14-64, msys2)
+ * Runtime:     62 ms
+ * Memory:      100 KB
+ * Difficulty:  800
+ * Solved:      2026-07-22 02:13:08
+ * URL:         https://codeforces.com/problemset/problem/4/A
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int w;
+    cin >> w;
+    
+    if (w > 2 && w % 2 == 0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    
+    return 0;
+}
